0cfc3b8f5b84bec3972cfc2ce9a8670905905c6a
Austin Chinn <achinn@ucsd.edu>
2016-02-29 02:28:09 -0800

    testing apidocs

--- route/business.route.js
+++ route/business.route.js
@@ -3,6 +3,22 @@
  */
 import * as business from '../controller/business.controller';
 
+/**
+ * @api {post} /business Create new business
+ * @apiName CreateBusiness
+ * @apiGroup Business
+ *
+ * @apiParam {Object} req Information stored in body: name, url, logo, description, owner
+ *
+ * @apiSuccessExample Example JSON on success:
+ * {
+ *   name:'Cool Hospital'
+ *   url:'coolhospital.org'  	
+ *   owner:'Dr. Cool'
+ * }
+ *
+ * @apiError MissingBody Missing name, url, or owner
+ */
 module.exports = function(app) {
   app.route('/business/new').post(business.newBusiness);
 };
