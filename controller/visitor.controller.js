8de00db3955bd22c1a58d432b49027d5d9e8f707
Andrei Khmelnitski <akhmelni@ucsd.edu>
2016-02-29 02:07:13 -0800

    added queue stuff for visitor, routes mostly done, not tested

--- controller/visitor.controller.js
+++ controller/visitor.controller.js
@@ -1,58 +1,32 @@
 import mongoose from 'mongoose';
+import moment from 'moment';
 var Visitor = mongoose.model('Visitor');
 
 export function newVisitor(req, res, next) {
+
   var missing = [];
-  console.log(req);
   if (!req.body.name)
     missing.push("missing name");
-  if (!req.body.businessId)
-    missing.push("need bussinesID")
   if (missing.length) {
     return res.status(400).send({
       "Error": missing.join(', ')
     });
   }
 
-  //need to implement security
-
-  Visitor.find({name: req.body.name})
-  .and(
-    [
-      {$or:
-        [
-          {phone: req.body.phone},
-          {email: req.body.email}
-        ]
-      }
-    ]).exec(function(err, visitor) {
-    if (err)
-      return res.status(400).send(err);
-    if (visitor) {
-      //will be replaced with either pushing ot not pushing to queue 
-      visitor.checkIns.push(new Date());
-      if (!req.body.requireCheckOff)
-        visitor.checkOffs.push(new Date());
-      if (req.body.form)
-        visitor.push(req.body.form);
-      visitor.save(function(err, updatedVisitor) {
-        if (err)
-          return res.status(400).send(err);
-        return res.status(200).send(updatedVisitor);
-      });
-    }
-  });
+  //change when we can get the business id from the user object
+  var businessId = 1;
 
   var newVisitor = new Visitor();
   newVisitor.name = req.body.name;
+  newVisitor.businessId = businessId;
   newVisitor.email = req.body.email || null;
   newVisitor.phone = req.body.phone || null;
-  if (req.body.form)
-    newVisitor.push(req.body.form);
-  //will change when we figure out how to do queue
+  newVisitor.form = req.body.form || null;
+
   newVisitor.checkIns.push(new Date());
+
   if (!req.body.requireCheckOff)
-   newVisitor.checkOffs.push(new Date());
+   newVisitor.checkOff = new Date();
   newVisitor.save(function(err, updatedVisitor) {
     if (err)
       return res.status(400).send(err);
@@ -61,27 +35,139 @@ export function newVisitor(req, res, next) {
 }
 
 export function checkOffVisitor(req, res, next) {
-  //will happen when we get queue
+
+  var businessId = 1;
+  var visitorId = req.params.visitorId;
+
+  Visitor.findById(visitorId, function(err, visitor) {
+    if (err)
+      return res.status(400).send(err);
+    if (visitor) {
+      if (visitor.businessId != businessId)
+        return res.status(400).send({"Error":"incorrect businessId"});
+      visitor.checkOff = new Date();
+      visitor.save(function(err, updatedVisitor) {
+        if (err)
+          return res.status(400).send(err);
+        return res.status(200).send(updatedVisitor);
+      });
+    }
+  });
+}
+
+export function getQueue(req, res, next) {
+  var businessId = 1;
+
+  var missing = [];
+  if (!req.query.page)
+    missing.push("missing page or page is 0");
+  if (!req.query.per_page)
+    missing.push("missing per_page or per_page is 0");
+  if (missing.length) {
+    return res.status(400).send({
+      "Error": missing.join(', ')
+    });
+  }
+
+  Visitor.find({businessId: businessId, checkOff: null})
+  .sort('-timeStamp.created')
+  .skip((req.query.page - 1) * req.query.per_page)
+  .limit(req.query.per_page) 
+  .exec(function (err, visitors) {
+    if (err)
+      return res.status(400).send(err);
+    return res.status(200).send(visitors);
+  });
+}
+
+export function getVisitors(req, res, next) {
+  
+  var businessId = 1;
+
+  var missing = [];
+  if (!req.query.page)
+    missing.push("missing page or page is 0");
+  if (!req.query.per_page)
+    missing.push("missing per_page or per_page is 0");
+  if (!req.query.date)
+    missing.push("missing date");
+  if (missing.length) {
+    return res.status(400).send({
+      "Error": missing.join(', ')
+    });
+  }
+
+  var startDate = moment(req.query.date).toDate();
+  var endDate = moment(req.query.date).add(1, 'days').toDate();
+
+  Visitor.find({
+    businessId: businessId, 
+    checkOff: {$ne: null}, 
+    checkIn: {
+      $gte: startDate,
+      $lte: endDate
+    }
+  })
+  .sort('-timeStamp.created')
+  .skip((req.query.page - 1) * req.query.per_page)
+  .limit(req.query.per_page) 
+  .exec(function (err, visitors) {
+    if (err)
+      return res.status(400).send(err);
+    return res.status(200).send(visitors);
+  });
+}
+
+export function deleteVisitor(req, res, next) {
+  var businessId = 1;
+  var visitorId = req.params.visitorId;
+
+  Visitor.findById(visitorId, function(err, visitor) {
+    if (err)
+      return res.status(400).send(err);
+    if (visitor) {
+      if (visitor.businessId != businessId)
+        return res.status(400).send({"Error":"incorrect businessId"});
+      visitor.remove(function(err) {
+        if (err)
+          return res.status(400).send(err);
+        return res.status(200).end();
+      });
+    }
+  });
 }
 
 export function search(req, res, next) {
   //next week
 }
 
-export function deleteVisitor(req, res, next) {
-  //need to implement security
 
-  var visitorId = req.query.id;
 
-  Visitor.findById(visitorId, function(err, visitor) {
-    if (err)
-      return res.status(500).send(err);
-    if (visitor)
-      visitor.remove(function(err) {
-        if (err)
-          return res.status(500).send(err);
-        return res.status(200).end();
-      });
-  });
+//here be shit code
 
-}
+  // Visitor.find({name: req.body.name})
+  // .and(
+  //   [
+  //     {$or:
+  //       [
+  //         {phone: req.body.phone},
+  //         {email: req.body.email}
+  //       ]
+  //     }
+  //   ]).exec(function(err, visitor) {
+  //   if (err)
+  //     return res.status(400).send(err);
+  //   if (visitor) {
+  //     //will be replaced with either pushing ot not pushing to queue 
+  //     visitor.checkIns.push(new Date());
+  //     if (!req.body.requireCheckOff)
+  //       visitor.checkOffs.push(new Date());
+  //     if (req.body.form)
+  //       visitor.push(req.body.form);
+  //     visitor.save(function(err, updatedVisitor) {
+  //       if (err)
+  //         return res.status(400).send(err);
+  //       return res.status(200).send(updatedVisitor);
+  //     });
+  //   }
+  // });
