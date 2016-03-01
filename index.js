75ac83813e7b7dee8e58c7079a66ac276826f7d6
Martin Gao <MartinGao@Martins-iMac.local>
2016-02-29 19:39:36 -0800

    CROS Support

--- index.js
+++ index.js
@@ -32,6 +32,13 @@ businessRoute(app);
 testRoute(app);
 visitorRoute(app);
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 
 app.use('/doc', express.static(__dirname + '/doc'));
 
