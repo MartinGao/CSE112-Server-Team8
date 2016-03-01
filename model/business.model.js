302df6e8b10a70b4f56776b556ca470879468b54
Martin Gao <MartinGao@Martins-iMac.local>
2016-02-29 16:36:04 -0800

    Finish User SignUp and SignIn

--- model/business.model.js
+++ model/business.model.js
@@ -1,44 +1,36 @@
 /**
- * Created by besin on 2/25/2016.
- */
+* Created by besin on 2/25/2016.
+*/
 /**
- * Schema for Business
- *
- */
+* Schema for Business
+*
+*/
 
 import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const BusinessSchema = new Schema({
-    //change to match schema
-    created: {
-        type: Date,
-        default: Date.now
-    },
-    name: {
-        type: String,
-        required: true
-    },
-    
-    //what url
-    url: {
-        type: String,
-        required: true
-    },
-    logo: {
-        type: String,
-        default: null
-    },
-    description: {
-        type: String,
-        default: null
-    },
-    owner: {
-        type: Schema.ObjectId,
-        ref: 'User',
-        required: true
-    }
-
+  timeStamp: {
+    created: { type: Date, default: Date.now },
+    updated: { type: Date, default: Date.now },
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  logo: {
+    type: String,
+    default: null,
+  },
+  description: {
+    type: String,
+    default: null,
+  },
+  owner: {
+    type: Schema.ObjectId,
+    ref: 'User',
+    default: null,
+  },
 });
 
 export default mongoose.model('Business', BusinessSchema);
