e6aa5c866f3a5d6610877469062951aed5c5042d
Dennis Bykkov <dbykkov@ucsd.edu>
2016-03-01 17:14:21 -0800

    Updated businesses.

--- route/business.route.js
+++ route/business.route.js
@@ -20,7 +20,25 @@ import * as business from '../controller/business.controller';
  *
  * @apiError MissingBody Missing name, url, or owner
  */
+
+/**
+ * @api {get} /business Get a business
+ * @apiName GetBusiness
+ * @apiGroup Business
+ *
+ * @apiParam {Object} req Information stored in body: name, url, logo, description, owner
+ *
+ * @apiSuccess {Object} business Returns the new business that was created
+ * @apiSuccessExample Example JSON on success:
+ * {
+ *   name:'Cool Hospital',
+ *   url:'coolhospital.org',
+ *   owner:'Dr. Cool'
+ * }
+ *
+ * @apiError MissingBody Missing name, url, or owner
+ */
 module.exports = function(app) {
-  app.route('/business/new').post(business.newBusiness);
+  app.route('/business').post(business.newBusiness);
   app.route('/business').get(business.getBusiness);
 };
