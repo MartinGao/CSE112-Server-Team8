a439597fea85a53b297a7b4e22aaf303868ac6ba
Dennis Bykkov <dbykkov@ucsd.edu>
2016-03-06 21:37:21 -0800

    Added to apiDocs.

--- route/form.route.js
+++ route/form.route.js
@@ -16,12 +16,23 @@ import * as form from '../controller/form.controller';
  *	   businessId: 678,
  *     field: { String: 'something' },
  *     timestamp: date
- *   }
+ *   }	
  * }
  *
  * @apiError MissingBody Missing businessID or form
  */
-module.exports = (app) => {
-  app.route('/form/').post(form.createForm);
-  app.route('/form/formId').delete(form.deleteForm);
+
+/**
+ * @api {delete} /form/formId Delete form
+ * @apiName CreateForm
+ * @apiGroup Form
+ *
+ * @apiParam {Object} id ID of form to delete set in the query.
+ *
+ * @apiError Error Missing formId
+ */
+
+module.exports = function(app) {
+	app.route('/form/').post(form.createForm);
+	app.route('/form/formId').delete(form.deleteForm);
 };
