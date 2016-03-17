556bf112523c25b636084faf47dd1011453f92b5
Martin Gao <Marting.Gao@Gmail.com>
2016-03-17 13:19:09 -0700

    Update index.js

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ mongoose.connect(TEST_DB, (err) => {
     console.log(chalk.green('Successfully connect to MongoDB!'));
     const port = process.env.PORT || 3000;
     var server = app.listen(port, function() {
-      console.log('Express server listening on port ' + PORT);
+      console.log('Express server listening on port ' + port);
     });
   }
 });
